9e7ad30279969e2b4effa00158e95a85f9a80456
Acoidán Betancor Arocha <123725691+imAko097@users.noreply.github.com>
2025-05-12 12:11:57 +0100

    SandwichSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Database\Seeders\ProductosSeeder\BocadilloSeeder;
+use Database\Seeders\ProductosSeeder\SandwichSeeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProductoTipoSeeder::class,
             BocadilloSeeder::class,
+            SandwichSeeder::class,
         ]);
     }
 }
